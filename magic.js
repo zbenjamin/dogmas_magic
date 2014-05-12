42e8bae05731bdf29b4ac45a05de0b15021647e6
Zev Benjamin <zev@mit.edu>
2014-05-11 23:30:57 -0700

    Add a base class to Spell for future Compound component types

--- magic.js
+++ magic.js
@@ -4,78 +4,89 @@ function Rune(name) {
     return this;
 }
 
-Rune.prototype = {
-    draw: function rune_draw(ctx, width, height) {
-        ctx.fillRect(0, 0, width, height);
-    }
-};
+Rune.prototype.draw = function rune_draw(ctx, width, height) {
+    ctx.fillRect(0, 0, width, height);
+}
 
 function max_component_complexity(components) {
     return _.pluck(components, 'complexity').reduce(Math.max);
 }
 
-function Spell(components) {
+function CompoundComponent(components) {
     this.components = components;
     this.complexity = Math.max(max_component_complexity(components) + 1,
                                Math.log(components.length - 1) / Math.log(2));
-    return this;
 }
 
-Spell.prototype = {
-    draw: function spell_draw(ctx, width, height) {
-        var radius = width / 2;
-        var i;
-        ctx.translate(width / 2, height / 2);
+CompoundComponent.prototype.draw = function compound_component_draw(ctx, width, height) {
+    this.draw_border(ctx, width, height);
 
-        // outer circle
-        ctx.beginPath();
-        ctx.arc(0, 0, radius, 0, Math.PI*2);
+    var layout = this.compute_layout(width, height);
+    var i;
+    for (i = 0; i < layout.dividers.length; i++) {
+        var divider = layout.dividers[i];
+        ctx.moveTo(divider.p1.x, divider.p1.y);
+        ctx.lineTo(divider.p2.x, divider.p2.y)
         ctx.stroke();
+    }
 
-        var layout = this.compute_layout(radius);
-        for (i = 0; i < layout.dividers.length; i++) {
-            var divider = layout.dividers[i];
-            ctx.moveTo(divider.p1.x, divider.p1.y);
-            ctx.lineTo(divider.p2.x, divider.p2.y)
-            ctx.stroke();
-        }
-        
-        for (i = 0; i < layout.regions.length; i++) {
-            var region = layout.regions[i];
-            ctx.save();
-            ctx.translate(region.x, region.y);
-            this.components[i].draw(ctx, region.width, region.width);
-            ctx.restore();
-        }
-    },
+    for (i = 0; i < layout.regions.length; i++) {
+        var region = layout.regions[i];
+        ctx.save();
+        ctx.translate(region.x, region.y);
+        this.components[i].draw(ctx, region.width, region.width);
+        ctx.restore();
+    }
+}
 
-    compute_layout: function spell_compute_layout(radius) {
-        var dividers = []
-        var regions = [];
+function Spell(components) {
+    CompoundComponent.call(this, components)
+}
 
-        if (this.components.length == 1) {
-            var max_width = Math.cos(Math.PI/4) * radius * 2;
-            regions.push({x: -1 * (max_width / 2 - max_width * 0.1),
-                          y: -1 * (max_width / 2 - max_width * 0.1),
-                          width: max_width * 0.8});
-        } else if (this.components.length == 2) {
-            dividers.push({p1: {x: Math.cos(Math.PI/4) * radius,
-                                y: -1 * Math.sin(Math.PI/4) * radius},
-                           p2: {x: -1 * Math.cos(Math.PI/4) * radius,
-                                y: Math.sin(Math.PI/4) * radius}});
+Spell.prototype = Object.create(CompoundComponent.prototype);
+Spell.prototype.constructor = Spell;
 
-            var max_width = Math.cos(Math.PI/4) * radius;
-            regions.push({x: -1 * (max_width - max_width * 0.1),
-                          y: -1 * (max_width - max_width * 0.1),
-                          width:  max_width * 0.8});
-            regions.push({x:      max_width * 0.1,
-                          y:      max_width * 0.1,
-                          width:  max_width * 0.8});
-        }
+Spell.prototype.draw = function spell_draw(ctx, width, height) {
+    ctx.save();
+    // for spells, it's a little easier to do things with the origin at the center
+    ctx.translate(width / 2, height / 2);
+    CompoundComponent.prototype.draw.call(this, ctx, width, height);
+    ctx.restore();
+}
 
-        return {dividers: dividers, regions: regions};
-    }    
-};
+Spell.prototype.draw_border = function spell_draw_border(ctx, width, height) {
+    ctx.beginPath();
+    ctx.arc(0, 0, width / 2, 0, Math.PI*2);
+    ctx.stroke();
+}
+
+Spell.prototype.compute_layout = function spell_compute_layout(width, height) {
+    var radius = width / 2;
+    var dividers = []
+    var regions = [];
+
+    if (this.components.length == 1) {
+        var max_width = Math.cos(Math.PI/4) * radius * 2;
+        regions.push({x: -1 * (max_width / 2 - max_width * 0.1),
+                      y: -1 * (max_width / 2 - max_width * 0.1),
+                      width: max_width * 0.8});
+    } else if (this.components.length == 2) {
+        dividers.push({p1: {x: Math.cos(Math.PI/4) * radius,
+                            y: -1 * Math.sin(Math.PI/4) * radius},
+                       p2: {x: -1 * Math.cos(Math.PI/4) * radius,
+                            y: Math.sin(Math.PI/4) * radius}});
+
+        var max_width = Math.cos(Math.PI/4) * radius;
+        regions.push({x: -1 * (max_width - max_width * 0.1),
+                      y: -1 * (max_width - max_width * 0.1),
+                      width:  max_width * 0.8});
+        regions.push({x:      max_width * 0.1,
+                      y:      max_width * 0.1,
+                      width:  max_width * 0.8});
+    }
+
+    return {dividers: dividers, regions: regions};
+}
 
 function parse_top(str) {
     var pos = 0;
