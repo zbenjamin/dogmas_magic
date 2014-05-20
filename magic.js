06e6e82695d606ba733e86ab02b3868f166d15cc
Zev Benjamin <zev@mit.edu>
2014-05-20 01:37:13 -0700

    Add Targets, Durations, and Restrictions

--- magic.js
+++ magic.js
@@ -27,6 +27,21 @@ function CompoundComponent(components) {
                                Math.log(components.length - 1) / Math.log(2));
 }
 
+CompoundComponent.prototype.draw_divider = function compound_component_draw_divider(ctx, width, height, divider) {
+    ctx.save();
+    ctx.moveTo(divider.p1.x, divider.p1.y);
+    ctx.lineTo(divider.p2.x, divider.p2.y)
+    ctx.stroke();
+    ctx.restore();
+}
+
+CompoundComponent.prototype.draw_subcomponent = function compound_component_draw_subcomponent(ctx, width, height, region, component) {
+    ctx.save();
+    ctx.translate(region.x, region.y);
+    component.draw(ctx, region.width, region.width);
+    ctx.restore();
+}
+
 CompoundComponent.prototype.draw = function compound_component_draw(ctx, width, height) {
     this.draw_border(ctx, width, height);
 
@@ -34,17 +49,13 @@ CompoundComponent.prototype.draw = function compound_component_draw(ctx, width, 
     var i;
     for (i = 0; i < layout.dividers.length; i++) {
         var divider = layout.dividers[i];
-        ctx.moveTo(divider.p1.x, divider.p1.y);
-        ctx.lineTo(divider.p2.x, divider.p2.y)
-        ctx.stroke();
+        this.draw_divider(ctx, width, height, divider);
     }
 
     for (i = 0; i < layout.regions.length; i++) {
         var region = layout.regions[i];
-        ctx.save();
-        ctx.translate(region.x, region.y);
-        this.components[i].draw(ctx, region.width, region.width);
-        ctx.restore();
+        var component = this.components[i];
+        this.draw_subcomponent(ctx, width, height, region, component);
     }
 }
 
@@ -130,6 +141,99 @@ Unit.prototype.compute_layout = function unit_compute_layout(width, height) {
     return {dividers: dividers, regions: regions};
 }
 
+function TriangularComponent(components, rotation_angle) {
+    this.rotation_angle = rotation_angle;
+    CompoundComponent.call(this, components);
+}
+
+TriangularComponent.prototype = Object.create(CompoundComponent.prototype);
+TriangularComponent.prototype.constructor = TriangularComponent;
+
+TriangularComponent.prototype.transform = function triangular_component_transform(ctx, width, height) {
+    ctx.translate(width / 2, height / 2);
+    ctx.rotate(this.rotation_angle);
+    ctx.translate(-width / 2, -height / 2);
+}
+
+TriangularComponent.prototype.draw_border = function triangular_component_draw_border(ctx, width, height) {
+    ctx.save();
+    this.transform(ctx, width, height);
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(width, 0);
+    ctx.lineTo(width / 2, height);
+    ctx.closePath();
+    ctx.stroke();
+
+    ctx.restore();
+}
+
+TriangularComponent.prototype.draw_divider = function triangular_component_draw_divider(ctx, width, height, divider) {
+    ctx.save();
+    this.transform(ctx, width, height);
+    CompoundComponent.prototype.draw_divider.call(this, ctx, width, height, divider);
+    ctx.restore();
+}
+
+TriangularComponent.prototype.draw_subcomponent = function triangular_component_draw_subcomponent(ctx, width, height, region, component) {
+    ctx.save();
+    this.transform(ctx, width, height);
+    ctx.translate(region.x, region.y);
+
+    // Un-rotate the the sub-component
+    ctx.translate(region.width / 2, region.width / 2);
+    ctx.rotate(-this.rotation_angle);
+    ctx.translate(-region.width / 2, -region.width / 2);
+
+    component.draw(ctx, region.width, region.width);
+    ctx.restore();
+}
+
+TriangularComponent.prototype.compute_layout = function triangular_component_compute_layout(width, height) {
+    var dividers = [];
+    var regions = [];
+
+    if (this.components.length == 1) {
+        var max_width = width * (1 - Math.sqrt(3)/(Math.sqrt(3) + 2));
+        regions.push({x: width * Math.sqrt(3) / (2 * Math.sqrt(3) + 4) + 0.1 * max_width,
+                      y: 0.1 * max_width,
+                      width: max_width * 0.8});
+    } else if (this.components.length == 2) {
+        dividers.push({p1: {x: width / 2, y: 0}, p2: {x: width / 2, y: height}});
+
+        var max_width = width / 2 - (width * width) / (2 * width + 4 * height);
+        regions.push({x: width / 2 - max_width * 0.9,
+                      y: max_width * 0.1,
+                      width: max_width * 0.8});
+        regions.push({x: width / 2 + max_width * 0.1,
+                      y: max_width * 0.1,
+                      width: max_width * 0.8});
+    } else {
+        throw new Error("TriangularComponents currently only support up to 2 components");
+    }
+
+    return {dividers: dividers, regions: regions};
+}
+
+function Target(components) {
+    TriangularComponent.call(this, components, 0);
+}
+Target.prototype = Object.create(TriangularComponent.prototype);
+Target.prototype.constructor = Target;
+
+function Duration(components) {
+    TriangularComponent.call(this, components, Math.PI);
+}
+Duration.prototype = Object.create(TriangularComponent.prototype);
+Duration.prototype.constructor = Duration;
+
+function Restriction(components) {
+    TriangularComponent.call(this, components, -Math.PI / 2);
+}
+Restriction.prototype = Object.create(TriangularComponent.prototype);
+Restriction.prototype.constructor = Restriction;
+
 function parse_magic(str) {
     var pos = 0;
     var ret;
@@ -151,7 +255,7 @@ function parse_magic(str) {
     return ret.tree;
 }
 
-function make_compound_component_parser(component_name, constructor, start_delim, end_delim) {
+function make_compound_component_parser(component_name, constructor, start_delim, end_delim, parse_prefix) {
     function parse_component(str, start) {
         if (str[start] != start_delim) {
             throw new Error("Not a " + component_name);
@@ -159,6 +263,17 @@ function make_compound_component_parser(component_name, constructor, start_delim
         var pos = start + 1;
         var ret;
         var components = []
+        var prefix;
+
+        if (parse_prefix) {
+            var regex = /(\w+):/g;
+            regex.lastIndex = pos;
+            prefix = regex.exec(str)[1];
+            if (prefix === undefined) {
+               throw new Error("Prefixed component has no prefix");
+            }
+            pos += prefix.length + 1;
+        }
 
         while (pos < str.length) {
             var c = str[pos];
@@ -179,7 +294,9 @@ function make_compound_component_parser(component_name, constructor, start_delim
                 components.push(ret.tree);
                 pos += ret.consumed;
             } else if (c == '<') {
-                throw new Error("Targets, durations, and restrictions are not yet supported");
+                ret = parse_triangular_component(str, pos);
+                components.push(ret.tree);
+                pos += ret.consumed;
             } else if (c == '{') {
                 throw new Error("Definitions are not yet supported");
             } else if (_.contains(')]}>', c)) {
@@ -194,13 +311,26 @@ function make_compound_component_parser(component_name, constructor, start_delim
         }
 
         return {consumed: pos - start,
-                tree: new constructor(components)};
+                tree: new constructor(components),
+                prefix: prefix};
     }
     return parse_component;
 }
 
-var parse_spell = make_compound_component_parser("spell", Spell, '(', ')');
-var parse_unit = make_compound_component_parser("unit", Unit, '[', ']');
+var parse_generic_triangular_component = make_compound_component_parser("triangular component", Object, '<', '>', true);
+function parse_triangular_component(str, start) {
+    var ret = parse_generic_triangular_component(str, start);
+    var ctors = {T: Target, D: Duration, R: Restriction};
+    if (! ctors.hasOwnProperty(ret.prefix)) {
+        throw new Error("Bad triangular component type: " + ret.prefix);
+    }
+
+    ret.tree = new ctors[ret.prefix](ret.tree);
+    return ret;
+}
+
+var parse_spell = make_compound_component_parser("spell", Spell, '(', ')', false);
+var parse_unit = make_compound_component_parser("unit", Unit, '[', ']', false);
 
 var svg_cache = {};
 function get_svg(url) {
